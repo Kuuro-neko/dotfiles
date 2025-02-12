c554cabd3a48de375e0e1b768c5b9ae728dd78ac
Gilles Gonzalez Oropeza <gilles.gonzalezoropeza@gmail.com>
2025-02-12 14:17:57 +0100

    Wofi allow images

--- hypr/.config/hypr/scripts/wofi.sh
+++ hypr/.config/hypr/scripts/wofi.sh
@@ -4,7 +4,7 @@ CONFIG="$HOME/.config/hypr/wofi/config/config"
 STYLE="$HOME/.config/hypr/wofi/src/mocha/style.css"
 
 if ! pgrep -x wofi >/dev/null; then
-    wofi --conf "${CONFIG}" --style "${STYLE}"
+    wofi --conf "${CONFIG}" --style "${STYLE}" --allow-images
 else
     pkill wofi
 fi
